df6da2f7a3e6e877c5c9d8e1c17051dacb5c98ad
cate <cate.boehm@oebb.at>
2026-06-18 08:54:26 +0200

    before switching client library

--- src/network_handler.rs
+++ src/network_handler.rs
@@ -6,7 +6,10 @@ use std::{borrow::Cow, hash::Hash, sync::Arc};
 use tokio::sync::{Mutex, MutexGuard};
 
 use crate::{
-    http_client_wrapper::{HttpClientWrapper, LoginState::LoggedOut},
+    http_client_wrapper::{
+        HttpClientWrapper,
+        LoginState::{self, LoggedOut},
+    },
     signalr_client_wrapper::SignalRClientWrapper,
 };
 
@@ -17,9 +20,24 @@ pub struct NetworkHandler {
 
 impl NetworkHandler {
     pub fn new() -> Self {
-        Self {
+        let temp = Self {
             http_client: Arc::new(Mutex::new(HttpClientWrapper::new())),
             signalr_client: Arc::new(Mutex::new(SignalRClientWrapper::new(LoggedOut))),
-        }
+        };
+        let pointer = temp.signalr_client.clone();
+        tokio::task::spawn(async move {
+            pointer.lock().await.connect(LoggedOut).await;
+        });
+        return temp;
+    }
+
+    pub async fn connect(&self, login_state: LoginState) {
+        let pointer = self.signalr_client.clone();
+        let pointer2 = pointer.clone();
+        tokio::task::spawn(async move {
+            pointer.lock().await.connect(LoggedOut).await;
+        })
+        .await;
+        if let Some(_) = pointer2.lock().await.client {}
     }
 }
--- src/signalr_client_wrapper.rs
+++ src/signalr_client_wrapper.rs
@@ -7,21 +7,22 @@ use crate::http_client_wrapper::LoginState::{self, LoggedIn, LoggedOut};
 
 //#[derive(Clone)]
 pub struct SignalRClientWrapper {
-    client: Option<SignalRClient>,
+    pub client: Option<SignalRClient>,
 }
 
 impl SignalRClientWrapper {
     pub fn new(login_state: LoginState) -> Self {
-        let mut temp = Self { client: None };
-        tokio::task::spawn({ temp.connect(login_state) });
-        return temp;
+        Self { client: None }
     }
 
     pub async fn connect(&mut self, login_state: LoginState) -> Option<SignalRClient> {
+        println!("attempting connection");
         let connection_result = SignalRClient::connect_with("localhost", "socket", |c| {
             if let LoggedIn(info) = login_state.clone() {
                 c.authenticate_bearer(info.accessToken);
             }
+            c.unsecure();
+            c.with_port(5244);
         })
         .await;
         match connection_result {
